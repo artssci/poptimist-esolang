d8e59e20ea84bfc5ab7f91ac5f6ab28c7d4486ce
Glitch (poptimism-language-finished) <none>
2025-02-26 14:52:12 +0000

    🗺️🍋 Updated with Glitch

--- vue.js
+++ vue.js
@@ -135,11 +135,9 @@ const grammar = `
     artist = "beyonce"/"queen"/ "claire" / "taylor"
     action = "plays"/"sings"/ "performs" / "dances"     
     video = "texas" { return "https://www.youtube.com/embed/238Z4YaAr1g?controls=0" } /     
-            "bohemian" { return "https://www.you-tube.com/embed/fJ9rUzIMcZQ?controls=0" }
-            
-            // /
-            // "bags" {return https://www.youtube.com/embed/L9HYJbe9Y18?controls=0"} /
-            // "blank space" {return "https://www.youtube.com/embed/v=e-ORhEE9VVg?controls=0"} 
+            "bohemian" { return "https://www.you-tube.com/embed/fJ9rUzIMcZQ?controls=0" } /
+            "bags" {return "https://www.youtube.com/embed/L9HYJbe9Y18?controls=0"} /
+            "blank space" {return "https://www.youtube.com/embed/v=e-ORhEE9VVg?controls=0"} 
 
 
 
